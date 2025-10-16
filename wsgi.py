54e61b40237c48f2ab42e9db888dce0e500de3b9
Sonstwer <AndreasCzak@gmail.com>
2025-10-16 15:59:29 +0200

    Fix server import; add production README for Gunicorn+systemd
    `server/__init__.py`: export only `app`
    `wsgi.py`: explicit WSGI entry
    `README.md`: install + systemd + proxy

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,2 @@
 # coding=utf-8
-from speed_friending_matcher import server
-from speed_friending_matcher.server import app as application
-
-server.configure(
-  input_plugin='csv:{}',
-  output_plugin='todo:{}:{}',
-  matchmaker='simple'
-)
+from speed_friending_matcher.server.server import app as application
